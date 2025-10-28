2cc537c5b6c9bfa89ca41c1afceb70b1cd3576f8
Atelz <igromen905@gmail.com>
2025-10-28 19:24:06 +0300

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,43 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Auto> autoList = new ArrayList<>();
+
+        int minSpeed = 0;
+        int maxSpeed = 250;
+
         for (int i = 0; i < 3; i++) {
             System.out.println("Введите название машины " + (i + 1));
             String autoName = scanner.next();
             int autoSpeed = -1;
+
             while (true) {
                 System.out.println("Введите скорость машины " + (i + 1));
                 if (scanner.hasNextInt()) {
                     autoSpeed = scanner.nextInt();
                 } else {
-                    scanner.next(); // Clear the invalid input
+                    scanner.next();
                 }
-                if (autoSpeed > 0 & autoSpeed <= 250)
+                if (autoSpeed > minSpeed & autoSpeed <= maxSpeed)
                     break;
                 System.out.println("Неправильная скорость");
             }
             autoList.add(new Auto(autoName, autoSpeed));
         }
+
         System.out.println("Победил " + Race.winDetector(autoList.get(0), autoList.get(1), autoList.get(2)));
-
-    }
-
-    static class Auto {
-        String name;
-        int speed;
-
-        Auto(String name, int speed) {
-            this.name = name;
-            this.speed = speed;
-        }
-    }
-
-    static class Race {
-        static Auto winner;
-
-        public static String winDetector(Auto car1, Auto car2, Auto car3) {
-            winner = (car1.speed * 24 > car2.speed * 24 ? (car1.speed * 24 > car3.speed * 24 ? car1 : car3) : (car2.speed * 24 > car3.speed * 24 ? car2 : car3));
-            return winner.name;
-        }
     }
 }
